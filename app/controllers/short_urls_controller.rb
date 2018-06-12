330db676c588c3d4b0f248555f1657027103ba35
Natalia Paguaga <npaguaga12@gmail.com>
2018-06-12 15:26:58 -0400

    change code controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,6 +2,6 @@ class ShortUrlsController < ApplicationController
 
   def show
     @website = Website.find(params[:id])
-    redirect_to "http://" + @website.url
+    redirect_to @website.url
   end
 end
